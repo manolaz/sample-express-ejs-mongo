e18414aec316c429727bbe58fe97b4caadbbdb3c
Tristan Nguyen <trungupdate@gmail.com>
2021-07-17 00:24:56 +0700

    connect mongo

---
+++ index.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://a:<password>@cluster0.2ariu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
